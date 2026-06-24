2199798eb1bb4d962697a5f5f0ab55f4d2d0b0b9
“Yu-Hsuan” <ball070428@gmail.com>
2026-06-24 19:59:30 +0800

    update 2026

--- Front-end/Front-end.js
+++ Front-end/Front-end.js
@@ -1,40 +1,52 @@
-let img = document.querySelector("img");
-let video = document.querySelector("video");
+const images = document.querySelectorAll(".image");
+const videos = document.querySelectorAll("video");
 
-let animation;
+let animation = [null, null];
 
-document
-  .querySelectorAll(".box")[0]
-  .addEventListener("mouseenter", function () {
+const imageBoxs = document.querySelectorAll(".imageBox");
+console.log(images);
+console.log(imageBoxs);
+imageBoxs.forEach((image, index) => {
+  image.addEventListener("mouseenter", () => {
     // 播放正向動畫
 
-    animation = img.animate(
+    animation[index] = images[index].animate(
       [{ objectPosition: "top" }, { objectPosition: "bottom" }],
       {
         duration: 5000,
         easing: "ease-in-out",
         fill: "forwards",
-      }
+      },
     );
   });
 
-document
-  .querySelectorAll(".box")[0]
-  .addEventListener("mouseleave", function () {
+  image.addEventListener("mouseleave", () => {
     // 播放倒轉動畫
-    if (animation) {
-      animation.reverse();
+    if (animation[index]) {
+      animation[index].reverse();
     }
-  });
-
-document.querySelectorAll(".box")[1].addEventListener("mouseover", function () {
-  video.play();
-  video.addEventListener("ended", () => {
-    video.currentTime = 0;
-    video.play();
   });
 });
 
-document.querySelectorAll(".box")[1].addEventListener("mouseout", function () {
-  video.pause();
+const videoBoxs = document.querySelectorAll(".videoBox");
+
+videos.forEach((video) => {
+  video.addEventListener("ended", () => {
+    video.currentTime = 0;
+    video.play().catch((e) => console.log("循環播放重啟失敗", e));
+  });
+});
+
+videoBoxs.forEach((videoBox, index) => {
+  videoBox.addEventListener("mouseover", async () => {
+    try {
+      await videos[index].play();
+    } catch (err) {
+      console.warn("自動播放被攔截，通常是因為尚未與網頁互動:", err);
+    }
+  });
+
+  videoBox.addEventListener("mouseout", () => {
+    videos[index].pause();
+  });
 });
